109052d51d9ee3c02af6392d41faf4da52d14971
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-02-15 21:11:19 -0400

    Time: 304 ms (62.27%), Space: 16.6 MB (87.41%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = defaultdict(int)
+        current, res = 0, 0
+        for i in range(len(nums)):
+            current += nums[i]
+            if current == k:
+                res += 1
+            if current - k in dic:
+                res += dic[current - k]
+            dic[current] += 1
+
+        return res
