dacc236fda9953fa96504ad7aa30e2223e9e9b59
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-02-24 20:44:39 -0400

    Time: 27 ms (94.11%), Space: 13.9 MB (92.33%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def appendZeros(self, version, cnt):
+        for i in range(cnt):
+            version.append("0")
+
+    def removeLeadingZeros(self, version):
+        for i in range(len(version)):
+            version[i] = int(version[i])
+
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1, v2 = version1.split("."), version2.split(".")
+        if len(v1) < len(v2):
+            self.appendZeros(v1, len(v2) - len(v1))
+        elif len(v2) < len(v1):
+            self.appendZeros(v2, len(v1) - len(v2))
+
+        self.removeLeadingZeros(v1)
+        self.removeLeadingZeros(v2)
+
+        for i in range(len(v1)):
+            if v1[i] < v2[i]:
+                return -1
+            elif v1[i] > v2[i]:
+                return 1
+        
+        return 0
+        
