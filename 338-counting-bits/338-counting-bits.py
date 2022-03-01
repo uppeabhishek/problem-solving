dfa38d69d415b7602b712e6d5cb8a51bed1bb7a0
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-02-28 21:40:49 -0400

    Time: 148 ms (40.27%), Space: 21 MB (20.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,38 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = []
-        for i in range(n + 1):
-            cur, cnt = i, 0
-            while cur:
-                cur = cur & (cur - 1)
-                cnt += 1
-            result.append(cnt)
-            
+        if n == 0:
+            return [0]
+        if n == 1:
+            return [0, 1]
+        if n == 2:
+            return [0, 1, 1]
+        if n == 3:
+            return [0, 1, 1, 2]
+
+        prev = [1, 2]
+        current = []
+
+        result = [0, 1, 1, 2]
+        
+        i = 4
+        while i <= n:
+            for k in range(0, len(prev)):
+                if i == n + 1:
+                    break
+                ele = prev[k] + 0
+                current.append(ele)
+                result.append(ele)
+                i += 1
+
+            for k in range(0, len(prev)):
+                if i == n + 1:
+                    break
+                ele = prev[k] + 1
+                current.append(ele)
+                result.append(ele)
+                i += 1
+
+            prev = current
+            current = []
+
         return result
