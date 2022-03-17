aaf3aa64249f033418e5d03b41a1d76c1e488d9f
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-03-16 23:12:39 -0300

    Time: 41 ms (59.02%), Space: 14 MB (30.17%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = []
+        for c in s:
+            if c == "(":
+                stack.append(c)
+            else:
+                current = 0
+                while len(stack):
+                    if stack[-1] == "(":
+                        stack.pop()
+                        break
+                    else:
+                        current += stack[-1]
+                        stack.pop()
+                if not current:
+                    stack.append(1)
+                else:
+                    stack.append(2 * current)
+
+        return sum(stack)
