dd82ad33738b9e7aa183d877794ba6e8b573d1ad
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-05-03 21:12:27 -0300

    Time: 341 ms (27.82%), Space: 15.3 MB (20.47%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -1,11 +1,49 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        nums1 = sorted(nums)
         left_i, right_i = -1, -2
-        for i in range(len(nums)):
-            if nums[i] != nums1[i]:
-                if left_i == -1:
-                    left_i = i
-                else:
-                    right_i = i
+        
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                left_i = i + 1
+                right_i = i + 1
+                break
+        
+        if left_i == -1:
+            return 0
+           
+        current = nums[left_i]
+        max_current_val = -sys.maxsize
+        
+        # find max after left index
+        j = left_i
+        
+        while j < len(nums):
+            if nums[j] < current:
+                current = nums[j]
+                left_i = j
+            j += 1
+    
+        j = left_i
+        
+        # find min in left index
+        while j > -1:
+            if nums[j] > current:
+                max_current_val = max(max_current_val, nums[j])
+                left_i = j
+            j -= 1
+        
+        # find max in right index
+        nums[right_i] = max_current_val
+        j = right_i
+                
+        while j < len(nums) - 1:
+            if nums[j] > nums[j + 1]:
+                nums[j + 1] = nums[j]
+                right_i = j + 1
+            j += 1
+        
         return right_i - left_i + 1
+                
+                
+        
+        
