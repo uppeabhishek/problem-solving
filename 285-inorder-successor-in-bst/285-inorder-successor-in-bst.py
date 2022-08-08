0ca4d1b6136ce98f0a8d812e71a80f1791532999
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-08 17:18:48 -0300

    Time: 107 ms (58.42%), Space: 18.7 MB (63.19%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
@@ -10,13 +10,18 @@ class Solution:
     def inorderSuccessorHelper(self, root, p):
         if root is None:
             return
-                
+        
         if root.val > p.val:
             if not self.inorderVal or root.val < self.inorderVal.val:
                 self.inorderVal = root
         
-        self.inorderSuccessorHelper(root.left, p)
-        self.inorderSuccessorHelper(root.right, p)
+        if root.val < p.val:
+            self.inorderSuccessorHelper(root.right, p)
+        elif root.val > p.val:
+            self.inorderSuccessorHelper(root.left, p)
+        else:
+            self.inorderSuccessorHelper(root.left, p)
+            self.inorderSuccessorHelper(root.right, p)
 
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
         self.inorderVal = None
