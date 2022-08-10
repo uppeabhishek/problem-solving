205485fd989b3dc46ed7e0a107873d7d2fc277bc
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-09 23:40:32 -0300

    Time: 151 ms (43.53%), Space: 30.4 MB (6.87%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,19 +1,26 @@
 class Solution:
+    
     def deleteAndEarn(self, nums: List[int]) -> int:
-        min_val = min(nums)
-        numbers = [0] * (max(nums) - min_val + 1)
-
-        if len(numbers) == 1:
-            return sum(nums)
-
-        for num in nums:
-            numbers[num - min_val] += num
-
-        dp = [0] * len(numbers)
-        dp[0] = numbers[0]
-        dp[1] = max(numbers[0], numbers[1])
-
-        for i in range(2, len(dp)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + numbers[i])
-
-        return dp[-1]
+        
+        c = Counter(nums)
+        
+        for key, value in c.items():
+            c[key] = key * value
+            
+        max_num = max(c)
+               
+        @cache
+        def dp(index):
+            
+            if index < 0:
+                return 0
+            
+            ele = c[index] if index in c else 0
+                
+            first = dp(index - 1)
+            second = ele + dp(index - 2)
+            
+            return max(first, second)
+            
+        
+        return dp(max_num)
