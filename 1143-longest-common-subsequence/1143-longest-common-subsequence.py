cff282a054efa02ee73b3dee8887024b6aafd558
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-10 17:14:33 -0300

    Time: 462 ms (84.13%), Space: 22.7 MB (58.50%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,21 +1,44 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
+        m, n = len(text1), len(text2)
+
         @cache
         def dp(i, j):
 
-            if i == len(text1) or j == len(text2):
+            if i == 0 or j == 0:
                 return 0
 
             first, second = 0, 0
 
-            if text1[i] == text2[j]:
-                first = 1 + dp(i + 1, j + 1)
+            if text1[i - 1] == text2[j - 1]:
+                first = 1 + dp(i - 1, j - 1)
             else:
-                first = dp(i + 1, j)
-                second = dp(i, j + 1)
+                first = dp(i - 1, j)
+                second = dp(i, j - 1)
 
             return max(first, second)
         
-        return dp(0, 0)
+        
+        def bottom_dp():
+            
+            matrix = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+                        
+            
+            for i in range(m + 1):
+                for j in range(n + 1):
+                    if i == 0 or j == 0:
+                        matrix[i][j] = 0
+                    elif text1[i - 1] == text2[j - 1]:
+                        matrix[i][j] = 1 + matrix[i - 1][j - 1]
+                    else:
+                        matrix[i][j] = max(matrix[i - 1][j], matrix[i][j-1])
+                    
+            return matrix[m][n]
+        
+        # return dp(m, n)
+        return bottom_dp()
+
+        
+                    
         
