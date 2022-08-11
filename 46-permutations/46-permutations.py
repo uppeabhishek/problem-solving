3a13e4468ea9b14c761bd1409fce7337cf6935fc
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-11 12:40:57 -0300

    Time: 70 ms (35.25%), Space: 14.1 MB (22.42%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,23 +1,25 @@
 from copy import copy
+
 class Solution:
-    
-    def swap(self, nums, i, j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
-        
-    def helper(self, nums, index):
-                
-        if index == len(nums):
-            self.result.append(copy(nums))
-    
-        for i in range(index, len(nums)):
-            self.swap(nums, i, index)
-            self.helper(nums, index + 1)
-            self.swap(nums, i, index)
-        
-    
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.result = []
-        self.helper(nums, 0)
-        return self.result
+
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+
+        result = []
+
+        def recursion(index):
+
+            if index == len(nums):
+                result.append(copy(nums))
+                return
+
+            for i in range(index, len(nums)):
+                swap(i, index)
+                recursion(index + 1)
+                swap(i, index)
+
+        recursion(0)
+        return result
