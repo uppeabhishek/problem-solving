38cf048631f51f90e22dd95ea5eee4cc2a44c509
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-11 20:03:41 -0300

    Time: 35 ms (90.51%), Space: 14.2 MB (20.05%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        def isValid(c):
+            if c[0] == '0':
+                return False
+            c = int(c)
+
+            return 1 <= c <= 26
+
+        @cache
+        def dp(i, j):
+
+            if i == len(s):
+                return 1
+            elif j == len(s):
+                return 0
+
+            first, second = 0, 0
+
+            if isValid(s[i]):
+                first = dp(i + 1, j + 1)
+
+            if i + 1 < len(s) and isValid(s[i: i + 2]):
+                second = dp(i + 2, j + 2)
+
+            return first + second
+
+        return dp(0, 0)
