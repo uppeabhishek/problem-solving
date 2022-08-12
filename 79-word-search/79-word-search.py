ed61b4df1a0dc9789ac1e4ded94e642ec0979656
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-12 18:37:45 -0300

    Time: 7268 ms (56.86%), Space: 14 MB (51.13%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,10 +1,9 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+
         m, n = len(board), len(board[0])
 
         first_letters = []
-        visited = set([])
 
         paths = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
@@ -12,23 +11,26 @@ class Solution:
             return not (i < 0 or j < 0 or i == m or j == n)
 
         def search_helper(i, j, k):
-
-            key = (i, j)
+            if board[i][j] != word[k]:
+                return False
 
             if k == len(word) - 1:
                 return True
 
-            visited.add(key)
+            board[i][j] = "#"
+
+            result = False
 
             for path in paths:
                 i1, j1 = path[0] + i, path[1] + j
-                if isValid(i1, j1) and (i1, j1) not in visited and board[i1][j1] == word[k + 1]:
-                    if search_helper(i1, j1, k + 1):
-                        return True
-            
-            visited.remove(key)
-            
-            return False
+                if isValid(i1, j1):
+                    result = search_helper(i1, j1, k + 1)
+                    if result:
+                        break
+
+            board[i][j] = word[k]
+
+            return result
 
         def search():
             for letter in first_letters:
