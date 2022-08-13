601d3243310f9a9134f91b146d8fa47aa01d9dc7
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-13 14:24:47 -0300

    Time: 588 ms (91.95%), Space: 14 MB (60.50%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,49 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        # odd length    
-        max_odd_len = 0
-        i = 0
-        max_odd_len_str = s[0]
-        while i < len(s):
-            cur_len = 0
-            
-            prev, nxt = i - 1, i + 1
-            
-            while prev > -1 and nxt < len(s) and s[prev] == s[nxt]:
-                prev -= 1
-                nxt += 1
-                cur_len += 2
-            
-            
-            if cur_len > max_odd_len:
-                max_odd_len_str = s[prev + 1:nxt]
-                max_odd_len = cur_len
-            
-            i += 1        
-    
-    
-        # even length
-        max_even_len = 0
-        i = 0
-        max_even_len_str = s[0]
-        while i < len(s):
-            cur_len = 0
-            
-            prev, nxt = i - 1, i
-            
-            while prev > -1 and nxt < len(s) and s[prev] == s[nxt]:
-                prev -= 1
-                nxt += 1
-                cur_len += 2
-            
-            if cur_len > max_even_len:
-                max_even_len_str = s[prev + 1:nxt]
-                max_even_len = cur_len
-            
-            i += 1        
-
-        if max_even_len > max_odd_len:
-            return max_even_len_str
         
-        return max_odd_len_str
+        n = len(s)
+        
+        max_len, max_val = 0, None
+        
+        for i in range(n):
+            
+            # even
+            
+            a, b = i, i
+            
+            while a > -1 and b < n:
+                if s[a] != s[b]:
+                    break
+                
+                if b - a + 1 > max_len:
+                    max_val = s[a: b + 1]
+                    max_len = b - a + 1
+                
+                a -= 1
+                b += 1
+            
+            # odd
+            
+            a, b = i, i + 1
+            while a > -1 and b < n:
+                if s[a] != s[b]:
+                    break
+                
+                if b - a + 1 > max_len:
+                    max_val = s[a: b + 1]
+                    max_len = b - a + 1
+                
+                a -= 1
+                b += 1
+        
+        return max_val
+        
