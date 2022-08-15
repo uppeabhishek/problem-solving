046b203c2800961d03fb184e5e110090d91a5ae0
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-15 18:51:42 -0300

    Time: 37 ms (87.59%), Space: 13.9 MB (63.35%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -3,21 +3,28 @@ class Solution:
         result = 0
 
         negative = False
+        
         if x < 0:
             negative = True
             x = x * -1
-
+            
+        negative_max_val = (2 ** 31) / 10
+        positive_max_val = (2 ** 31 - 1) / 10
+        
         while x != 0:
             rem = x % 10
             result = result * 10 + rem
+            
+            if negative:
+                if result / 10 > negative_max_val:
+                    return 0
+            else:
+                if result / 10 > positive_max_val:
+                    return 0
+                
             x = x // 10
-
+        
         if negative:
             result = result * -1
-            if result < -2 ** 31:
-                return 0
-        else:
-            if result > 2 ** 31 - 1:
-                return 0
-        
+            
         return result
