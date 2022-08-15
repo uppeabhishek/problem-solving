228ef27ce33306934d01a58875b007b51d4d2a71
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-14 21:06:55 -0300

    Time: 70 ms (43.53%), Space: 13.9 MB (25.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,24 +3,53 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 1
-        while i > 0:
-            if nums[i] > nums[i - 1]:
-                break
-            i -= 1
-
-        if i == 0:
-            nums.sort()
-        else:
-            current = nums[i - 1]
-            min_val = math.inf
-            min_ind = -1
-            for j in range(i, len(nums)):
-                if current < nums[j] < min_val:
-                    min_val = min(min_val, nums[j])
-                    min_ind = j
-
-            nums[i - 1], nums[min_ind] = nums[min_ind], nums[i - 1]
-            nums[i:] = sorted(nums[i:])
-            
+    
+        n = len(nums)        
                 
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            
+        
+        def nextGreaterElement(ele, i):
+            
+            minElement, minIndex = sys.maxsize, -1
+
+            for j in range(i, n):
+                if nums[j] > ele and nums[j] < minElement:
+                    minElement = nums[j]
+                    minIndex = j
+            
+            return minIndex
+        
+        def helper():
+            
+            nonlocal nums
+            
+            i = n - 1
+        
+            while i > 0:
+                if nums[i] > nums[i - 1]:
+                    break
+                i -= 1
+
+            if i == 0:
+                nums.sort()
+                return
+            
+            index = nextGreaterElement(nums[i - 1], i)
+            
+            swap(i - 1, index)
+            
+            
+            temp = nums[0:i] + sorted(nums[i:])
+            
+            for i in range(n):
+                nums[i] = temp[i]
+                
+            
+        helper()
+                
+            
+        
