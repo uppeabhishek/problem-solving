254e2ac3e3dcee9251540e01fc3bc71e7cda5549
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-16 17:57:51 -0300

    Time: 90 ms (90.54%), Space: 20.8 MB (30.64%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,38 +1,49 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        
+        
         if n == 0:
             return [0]
+        
         if n == 1:
             return [0, 1]
+        
         if n == 2:
             return [0, 1, 1]
+        
         if n == 3:
             return [0, 1, 1, 2]
-
-        prev = [1, 2]
-        current = []
+    
+    
+        cache = [1, 2]
+        cnt = 3
 
         result = [0, 1, 1, 2]
         
-        i = 4
-        while i <= n:
-            for k in range(0, len(prev)):
-                if i == n + 1:
+        while cnt <= n:
+                        
+            new_cache = []
+            
+            for i in range(len(cache)):
+                shouldBreak = False
+                
+                cnt += 1
+                new_cache.append(cache[i] + 0)
+                if cnt == n:
+                    cnt += 1
                     break
-                ele = prev[k] + 0
-                current.append(ele)
-                result.append(ele)
-                i += 1
-
-            for k in range(0, len(prev)):
-                if i == n + 1:
+                    
+                cnt += 1
+                new_cache.append(cache[i] + 1)
+                if cnt == n:
+                    cnt += 1
                     break
-                ele = prev[k] + 1
-                current.append(ele)
-                result.append(ele)
-                i += 1
-
-            prev = current
-            current = []
-
+                
+            cache = new_cache
+            result += cache
+                
         return result
+        
+        
+                    
+                    
