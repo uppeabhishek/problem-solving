58ca098a3abc71281b7bb6545de3c2f5d25e5a79
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-18 20:56:16 -0300

    Time: 1055 ms (80.28%), Space: 63.2 MB (43.48%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,8 +1,34 @@
 class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
+    def firstMissingPositive(self, nums: List[int]) -> int:    
         
-        se = set(nums)
+        min_val, max_val = sys.maxsize, -sys.maxsize
         
-        for i in range(1, 2 ** 31):
-            if i not in se:
-                return i
+        for num in nums:
+            if num > 0:
+                if num < min_val:
+                    min_val = num
+                if num > max_val:
+                    max_val = num
+        
+        if min_val != 1:
+            return 1
+        
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:    
+                nums[i] = 1
+                
+        for i in range(len(nums)):
+            index = nums[i]
+            nxt = nums[abs(index) - 1]
+                        
+            if nxt > 0:
+                nums[abs(index) - 1] = -1 * nxt
+        
+        for i, num in enumerate(nums):
+            if num > 0:
+                return i + 1
+        
+        
+        return max_val + 1
