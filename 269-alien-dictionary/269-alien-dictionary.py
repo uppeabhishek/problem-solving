4a218f366977a47d44ebddea35af7af19d660cde
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-21 19:15:44 -0300

    Time: 51 ms (57.84%), Space: 14.1 MB (10.17%) - LeetHub

---
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -0,0 +1,87 @@
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(set)
+    
+    def addEdge(self, u, v):
+        self.graph[u].add(v)
+
+    def DFSCycleHelper(self, v, visited, recursion):
+        visited.add(v)
+        recursion.add(v)
+        
+        
+        for u in self.graph[v]:
+            if u not in visited:
+                if self.DFSCycleHelper(u, visited, recursion):
+                    return True
+            elif u in recursion:
+                return True
+        
+        recursion.remove(v)
+        return False    
+        
+        
+    def DFSCycle(self):
+        visited = set()
+        recursion = set()
+        
+        for u in self.graph:
+            if u not in visited:
+                if self.DFSCycleHelper(u, visited, recursion):
+                    return True
+    
+        return False
+
+    def DFSHelper(self, v, result, visited):
+        visited.add(v)
+        
+        for u in self.graph[v]:
+            if u not in visited:
+                self.DFSHelper(u, result, visited)
+
+        result.appendleft(v)
+        
+    def DFS(self):
+        result = deque([])
+        visited = set()
+                        
+        for u in self.graph:
+            if u not in visited:
+                self.DFSHelper(u, result, visited)
+        
+        return "".join(result)
+    
+    def getGraph(self):
+        return self.graph
+            
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        
+        g = Graph()
+        
+        graph = g.getGraph()
+        
+        empty = True
+        
+        for w in words:
+            for c in w:
+                g.graph[c] = set()
+        
+        
+        for i in range(len(words) - 1):
+            l1, l2 = len(words[i]), len(words[i + 1])
+            min_len = min(l1, l2)
+            
+            if l1 > l2 and words[i][0:min_len] == words[i + 1][0:min_len]:
+                return ""
+            
+            for j in range(min_len):   
+                current, nxt = words[i][j], words[i + 1][j]
+                if current != nxt:
+                    g.addEdge(words[i][j], words[i + 1][j])
+                    break
+        
+        if g.DFSCycle():
+            return ""
+                
+        return g.DFS()
