7d8b0c495c6ee71d194e70ea4c0ac3b8b995bda8
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-08-25 01:12:13 -0300

    Time: 64 ms (51.31%), Space: 14.1 MB (96.01%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -3,29 +3,14 @@ class Solution:
         
         n = len(nums)
         
-        def bsearch(low, high):
-                        
-            if low == high:
-                return nums[low]
-            
-            mid = low + (high - low) // 2
-            
-            if nums[mid] <= nums[low] and nums[mid] <= nums[high]:
-                if nums[mid] < nums[mid - 1] and nums[mid] < nums[mid + 1]:
-                    return nums[mid]
-                if nums[mid - 1] < nums[mid + 1]:
-                    return bsearch(low, mid - 1) 
+        low, high = 0, n - 1
 
-                return bsearch(mid + 1, high)
-
-            elif nums[mid] > nums[low] and nums[mid] > nums[high]:
-                if nums[low] < nums[high]:
-                    return bsearch(low, mid - 1)
-                return bsearch(mid + 1, high)
-            elif nums[mid] > nums[low]:
-                return bsearch(low, mid - 1)
-            return bsearch(mid + 1, high)
-            
-        return bsearch(0, n - 1)
-            
-        
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] < nums[high]:
+                high = mid
+            else:
+                low = mid + 1
+                    
+        return nums[high]
+           
