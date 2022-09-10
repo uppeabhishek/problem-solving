400635c076a14734bf8074ae03680fe3cf5e953a
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-09-10 20:44:49 -0300

    Time: 59 ms (29.48%), Space: 14 MB (16.08%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -2,8 +2,16 @@ class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
         max_res = 0
         for i, n in enumerate(number):
-            if n == digit:
-                max_res = max(max_res, int(number[0:i] + number[i + 1:]))
+            if n == digit and i + 1 < len(number) and number[i + 1] > digit:
+                max_res = number[0:i] + number[i + 1:]
+                break
         
-        return str(max_res)
+        if not max_res:
+            for i, n in enumerate(number):
+                if n == digit:
+                    last_index = i
+            
+            return number[0:last_index] + number[last_index + 1:]
+        
+        return max_res
                 
