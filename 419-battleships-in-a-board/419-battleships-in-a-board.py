0995de4985b61e5c3f04a26b772f6580a4c0a59a
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-09-14 19:37:21 -0300

    Time: 67 ms (99.55%), Space: 14.4 MB (93.99%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -1,30 +1,12 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         
-        def valid(i, j):
-            return not (i < 0 or j < 0 or i == len(board) or j == len(board[0])) and board[i][j] != '.'
-        
-        def helper(i, j):
-            
-            if board[i][j] == '.':
-                return 
-            
-            board[i][j] = '.'
-            
-            paths = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-            
-            for path in paths:
-                i1, j1 = path[0] + i, path[1] + j
-                if valid(i1, j1):
-                    helper(i1, j1)
-            
         
         result = 0
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == 'X':
-                    helper(i, j)
+                if board[i][j] == 'X' and (i == 0 or board[i - 1][j] != 'X') and (j == 0 or board[i][j - 1] != 'X'):
                     result += 1
         
         return result
