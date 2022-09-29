9ef4ab305c2553c7bfc4c678bbbbaa3bdafd2f99
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-09-29 01:01:16 -0300

    Time: 450 ms (94.23%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select e.employee_id from Employees e left outer join Salaries s on e.employee_id = s.employee_id where s.salary is null union select s.employee_id from Employees e right outer join Salaries s on e.employee_id = s.employee_id where e.name is null order by employee_id; 
