46feb7f54c156691d42c723677e1c9e622e12f24
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-09-29 00:33:32 -0300

    Time: 57 ms (41.46%), Space: 14 MB (19.54%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,23 +1,11 @@
 class Solution:
-    
-    def robHelper(self, nums, index, cache):
-        
-        if index in cache:
-            return cache[index]
-        
-        if index == 0:
-            return nums[index]
-    
-        if index == 1:
-            return max(nums[index], nums[index - 1])
-        
-        first = self.robHelper(nums, index - 1, cache)
-        second = nums[index] + self.robHelper(nums, index - 2, cache)
-        
-        cache[index] = max(first, second)
-        
-        return cache[index]
-    
     def rob(self, nums: List[int]) -> int:
-        cache = {}
-        return self.robHelper(nums, len(nums) - 1, cache)
+        
+        @cache
+        def helper(i):
+            if i < 0:
+                return 0
+            
+            return max(helper(i - 1), nums[i] + helper(i - 2))
+        
+        return helper(len(nums) - 1)
