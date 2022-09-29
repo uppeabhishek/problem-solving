62733911a8650de1d5cefaaa9a7afcf4915bb9c9
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-09-29 01:44:58 -0300

    Time: 447 ms (71.16%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 
-select id,
+select DISTINCT t1.id,
 case 
-    when p_id is null then 'Root'
-    when id in (select p_id from Tree) then 'Inner'
+    when t1.p_id is null then 'Root'
+    when t1.id = t2.p_id then 'Inner'
     else 'Leaf'
 end as type
-from Tree;
+from Tree t1 LEFT JOIN Tree t2 ON t1.id = t2.p_id;
