ca29b0bc10250156fab49cd5ddf5df81811cfb28
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-10-05 04:31:28 -0300

    Time: 1480 ms (63.38%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select product_id, product_name from (select p.product_id, (case when sum(case when s.sale_date >= '2019/01/01' and s.sale_date <= '2019/03/31' then 1 else 0 end) = count(s.product_id) then p.product_name end) as product_name from Product p JOIN Sales s on p.product_id = s.product_id group by s.product_id) as t1 where product_name is not null;
