e631cabf64d6f2894c995a5bd396fedf06faa1e5
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-10-08 22:46:37 -0300

    Time: 684 ms (34.93%), Space: 0B (100.00%) - LeetHub

---
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select round(count(*) / (select count(delivery_id) from Delivery) * 100, 2) as immediate_percentage from Delivery where order_date = customer_pref_delivery_date;
