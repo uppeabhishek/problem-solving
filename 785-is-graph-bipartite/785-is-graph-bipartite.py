749cd91b7df04053ed43a8cc1a8fd4b8d7e0843d
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-10-10 03:08:07 -0300

    Time: 401 ms (37.28%), Space: 14.5 MB (49.10%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,65 +1,33 @@
-from collections import defaultdict
-from typing import List
-from copy import copy
-
-class Graph:
-    def __init__(self, n, graph):
-        self.n = n
-        self.graph = graph
-        self.set1, self.set2 = set(), set()
-
-
-    def DFSHelper(self, u, visited, isSet1):
-
-        visited.add(u)
-
-        for v in self.graph[u]:
-            if isSet1:
-                if u in self.set2:
-                    return False
-                if v in self.set1:
-                    return False
-
-                self.set1.add(u)
-                self.set2.add(v)
-            else:
-                if u in self.set1:
-                    return False
-                if v in self.set2:
-                    return False
-
-                self.set2.add(u)
-                self.set1.add(v)
-
-            if v not in visited:
-                if not self.DFSHelper(v, visited, not isSet1):
-                    return False
-
-        return True
-
-    def DFS(self):
-        visited = set()
-        
-        for i in range(self.n):
-            if i not in visited:    
-                if not self.DFSHelper(i, visited, True):
-                    
-                    self.set1, self.set2 = set(), set()
-                    
-                    visited = set()
-                    
-                    if not self.DFSHelper(i, visited, False):    
-                        return False
-                
-        
-        return True
-
-    
 class Solution:
+    
+    def DFS(self, u, color, graph):
+        
+        if u not in color:
+            color[u] = 0
+        
+        for v in graph[u]:
+            if v not in color:
+                color[v] = color[u] ^ 1
+                if not self.DFS(v, color, graph):
+                    return False
+            elif color[v] == color[u]:
+                return False
+            
+        return True
+        
+        
+        
     def isBipartite(self, graph: List[List[int]]) -> bool:
         
-        g = Graph(len(graph), graph)
+        color = defaultdict(int)
         
-        return g.DFS()        
+        n = len(graph)
+        
+        for i in range(n):
+            if not self.DFS(i, color, graph):
+                return False
+        
+        return True
+            
         
         
