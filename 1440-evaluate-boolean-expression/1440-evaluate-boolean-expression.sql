2ec89e0ba397bef7b60d0cda0ed2c7209a55232b
Abhishek Uppe <uppeabhishek97@gmail.com>
2022-10-12 20:26:48 -0300

    Time: 1529 ms (55.72%), Space: 0B (100.00%) - LeetHub

--- 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
+++ 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
@@ -1,9 +1,16 @@
 # Write your MySQL query statement below
 
+# select 
+    # left_operand, operator, right_operand, (case 
+    #     when operator = '=' then (case when l = r then 'true' else 'false' end) 
+    #     when operator = '<' then (case when l < r then 'true' else 'false' end) 
+    #     else (case when l > r then 'true' else 'false' end) 
+    # end) as value
+#         from (select *, (select value from Variables where name = left_operand) as l, (select value from Variables where name = right_operand) as r from Expressions) as t;
+
 select 
     left_operand, operator, right_operand, (case 
-        when operator = '=' then (case when l = r then 'true' else 'false' end) 
-        when operator = '<' then (case when l < r then 'true' else 'false' end) 
-        else (case when l > r then 'true' else 'false' end) 
-    end) as value
-        from (select *, (select value from Variables where name = left_operand) as l, (select value from Variables where name = right_operand) as r from Expressions) as t;
+        when operator = '=' then (case when v1.value = v2.value then 'true' else 'false' end) 
+        when operator = '<' then (case when v1.value < v2.value then 'true' else 'false' end) 
+        else (case when v1.value > v2.value then 'true' else 'false' end) 
+    end) as value from Expressions e JOIN Variables v1 on e.left_operand = v1.name JOIN Variables v2 on e.right_operand = v2.name;
